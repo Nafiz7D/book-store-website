3642dbc25ce6822fc0675a58eb579a1d20f5b4a8
unknown <mrnafsu07@gmail.com>
2025-03-01 13:37:08 +0600

    added appstorefile

--- src/components/AppStoreBanner/AppStoreBanner.jsx
+++ src/components/AppStoreBanner/AppStoreBanner.jsx
@@ -1,47 +1,54 @@
 import React from "react";
-import BoardImg from "../../assets/website/board.png";
-import PlayStoreImg from "../../assets/website/play_store.png";
-import AppStoreImg from "../../assets/website/app_store.png";
+import AppStoreImg from "../../assets/app_store.png";
+import PlayStoreImg from "../../assets/play_store.png";
+import Banner from "../../assets/website/board.png";
 
 const bannerImg = {
-  backgroundImage: `url(${BoardImg})`,
+  backgroundImage: `url(${Banner})`,
   backgroundPosition: "center",
   backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
   height: "100%",
   width: "100%",
 };
 
-const AppStoreBanner = () => {
+const AppStore = () => {
   return (
-    <div
-      className="bg-gray-100 dark:bg-gray-800 text-white py-10"
-      style={bannerImg}
-    >
-      <div className="container">
-        <div className="space-y-6 max-w-xl mx-auto">
-          <h1 className="text-2xl text-center sm:text-4xl font-semibold">
-            Read Books at your fingertips
-          </h1>
-          <div className="flex flex-wrap justify-center items-center gap-4">
-            <a href="#">
-              <img
-                className="max-w-[150px] sm:max-w-[120px] md:max-w-[200px]"
-                src={PlayStoreImg}
-                alt=""
-              />
-            </a>
-            <a href="#">
-              <img
-                className="max-w-[150px] sm:max-w-[120px] md:max-w-[200px]"
-                src={AppStoreImg}
-                alt=""
-              />
-            </a>
+    <>
+      <div
+        className="bg-gray-100 dark:bg-gray-800 text-white py-10"
+        style={bannerImg}
+      >
+        <div className="container">
+          <div
+            data-aos="fade-up"
+            data-aos-duration="300"
+            className="space-y-6 max-w-xl mx-auto"
+          >
+            <h1 className="text-2xl text-center sm:text-left sm:text-4xl font-semibold ">
+              Read Books at Your Fingertips
+            </h1>
+            <div className="flex flex-wrap justify-center items-center">
+              <a href="#">
+                <img
+                  src={PlayStoreImg}
+                  alt="Play store"
+                  className="max-w-[150px] sm:max-w-[120px] md:max-w-[200px]"
+                />
+              </a>
+              <a href="#">
+                <img
+                  src={AppStoreImg}
+                  alt="App store"
+                  className="max-w-[150px] sm:max-w-[120px] md:max-w-[200px]"
+                />
+              </a>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
-export default AppStoreBanner;
+export default AppStore;
